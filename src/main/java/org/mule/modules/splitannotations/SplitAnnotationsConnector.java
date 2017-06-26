a62587ef7149611ef0a1ad870737dfbc0061c2fb
Ankit Jain <ankitjainst@gmail.com>
2017-06-26 15:09:22 +0000

    Initial Commit

---
+++ src/main/java/org/mule/modules/splitannotations/SplitAnnotationsConnector.java
@@ -0,0 +1,37 @@
+package org.mule.modules.splitannotations;
+
+import org.mule.api.annotations.Config;
+import org.mule.api.annotations.Connector;
+import org.mule.api.annotations.Processor;
+
+import org.mule.modules.splitannotations.config.ConnectorConfig;
+
+@Connector(name="split-annotations", friendlyName="SplitAnnotations")
+public class SplitAnnotationsConnector {
+
+    @Config
+    ConnectorConfig config;
+
+    /**
+     * Custom processor
+     *
+     * @param friend Name to be used to generate a greeting message.
+     * @return A greeting message
+     */
+    @Processor
+    public String greet(String friend) {
+        /*
+         * MESSAGE PROCESSOR CODE GOES HERE
+         */
+        return config.getGreeting() + " " + friend + ". " + config.getReply();
+    }
+
+    public ConnectorConfig getConfig() {
+        return config;
+    }
+
+    public void setConfig(ConnectorConfig config) {
+        this.config = config;
+    }
+
+}
---
+++ src/main/java/org/mule/modules/splitannotations/config/ConnectorConfig.java
@@ -0,0 +1,57 @@
+package org.mule.modules.splitannotations.config;
+
+import org.mule.api.annotations.components.Configuration;
+import org.mule.api.annotations.Configurable;
+import org.mule.api.annotations.param.Default;
+import org.mule.modules.BaseConfig;
+
+@Configuration(friendlyName = "Configuration")
+public class ConnectorConfig extends BaseConfig{
+
+    /**
+     * Greeting message
+     */
+    @Configurable
+    @Default("Hello")
+    private String greeting;
+
+    /**
+     * Reply message
+     */
+    @Configurable
+    @Default("How are you?")
+    private String reply;
+
+    /**
+     * Set greeting message
+     *
+     * @param greeting the greeting message
+     */
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    /**
+     * Get greeting message
+     */
+    public String getGreeting() {
+        return this.greeting;
+    }
+
+    /**
+     * Set reply
+     *
+     * @param reply the reply
+     */
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+    /**
+     * Get reply
+     */
+    public String getReply() {
+        return this.reply;
+    }
+
+}
